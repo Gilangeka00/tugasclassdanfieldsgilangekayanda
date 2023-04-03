b73a384b5d96005a78b06761dde37214f61a34f8
gilang <gilangekayanda@students.amikom.ac.id>
2023-04-03 22:19:11 +0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classdanobject03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new classdanobject03.Mobil
+            {
+                Nama = "saya",
+                Warna = "Putih",
+                Merek = "Mitsubishi",
+                Model = "Xpander",
+                Tahun = 2017,
+                Jmlpintu = 4
+            };
+
+            mobil1.Gas(170);
+            mobil1.Klakson("Tut-tut-tut");
+            mobil1.TampilkanInfo();
+
+            Console.WriteLine("Tekan enter untuk keluar . . .");
+            Console.ReadKey();
+        }
+    }
+}
